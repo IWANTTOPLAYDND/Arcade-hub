0bc53b2b01bebcbcc0d5f830b35ccad51176c0bb
Erinnn <EDeJesus671@paps.net>
2026-06-28 21:22:56 -0400

    Add buttons to select different wallpapers

--- js/settings.js
+++ js/settings.js
@@ -1,1 +1,21 @@
+<h2>Wallpapers</h2>
 
+<button onclick="Wallpapers.set('wallpapers/purple.jpg')">
+💜 Purple
+</button>
+
+<button onclick="Wallpapers.set('wallpapers/blue.jpg')">
+💙 Blue
+</button>
+
+<button onclick="Wallpapers.set('wallpapers/pink.jpg')">
+🩷 Pink
+</button>
+
+<button onclick="Wallpapers.set('wallpapers/galaxy.jpg')">
+🌌 Galaxy
+</button>
+
+<button onclick="Wallpapers.set('wallpapers/forest.jpg')">
+🌲 Forest
+</button>
